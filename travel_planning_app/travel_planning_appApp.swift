31d46d0a65c278177cd808f79c5fd39740f46f4a
Matthew Macalalad <matthew.macalalad@georgebrown.ca>
2026-02-07 14:40:46 -0500

    Initial Commit

---
+++ travel_planning_app/travel_planning_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  travel_planning_appApp.swift
+//  travel_planning_app
+//
+//  Created by Matthew Macalalad on 2026-02-07.
+//
+
+import SwiftUI
+
+@main
+struct travel_planning_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
